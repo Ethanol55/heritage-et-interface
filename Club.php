a9047164fa32d59e115f5996b026587fad47bfa4
Ethan <ethanbellaiche0@gmail.com>
2022-02-14 14:41:07 +0100

    classe lien club

--- Club.php
+++ Club.php
@@ -27,7 +27,7 @@ class Club implements Comparable
      */
     public function AjouterSport(Sport $sport)
     {
-        $this->lesSports = $sport;
+        $this->lesSports[] = $sport;
     }
 
     /**
